d114ef897c3a7adcbf12fbfc1d82312258a9d151
adrianitargaona <adrianitargaona@gmail.com>
2019-04-04 10:11:22 +0200

    Add project files.

---
+++ PE2.AdrianaRodriguez.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PE2.AdrianaRodriguez.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
