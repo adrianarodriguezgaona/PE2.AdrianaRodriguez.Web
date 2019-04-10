dc3c9d62794863e9402cfc056cd8f621e28ae2c9
adrianitargaona <adrianitargaona@gmail.com>
2019-04-10 12:36:19 +0200

    Na deze aanpassing heb ik 3 ViewBag met als value elke een IEnumerable 1 ViewBag van Province, 1 ViewBag Geography en  1 ViewBag Race. De Ienumerables zijn alfabetisch gesoorteerd per naam

--- PE2.AdrianaRodriguez.Web/Controllers/HomeController.cs
+++ PE2.AdrianaRodriguez.Web/Controllers/HomeController.cs
@@ -5,13 +5,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PE2.AdrianaRodriguez.Web.Models;
+using Tamriel.Domain;
 
 namespace PE2.AdrianaRodriguez.Web.Controllers
+
 {
-    public class HomeController : Controller
+public class HomeController : Controller
     {
         public IActionResult Index()
+
         {
+            //List<Province> provinceList = new List<Province>();
+            var provinceRepository = new ProvinceRepository();
+            var geographyRepository = new GeographyRepository();
+            var raceRepository = new RaceRepository();
+
+            ViewBag.ProvinceList = provinceRepository.GetProvinces().OrderBy(Province => Province.Name);
+            ViewBag.GeographyList = geographyRepository.GetGeography().OrderBy(Geography => Geography.Name);
+            ViewBag.RaceList = raceRepository.GetRaces().OrderBy(Race => Race.Name);
+
             return View();
         }
 
